150eec8e1a51b223fd389d53a82047f65971ab23
PalloAbhi <pallavikhandekar212@gmail.com>
2014-03-29 18:53:22 -0700

    Segmentation code

---
+++ ImageSegmentation.R
@@ -0,0 +1,13 @@
+#Use follwing commands to install package.
+#source("http://bioconductor.org/biocLite.R")
+#biocLite("CRImage")
+#Download and save the sample images folder in current directory
+#setwd("<currentdirectory path>")
+
+library("CRImage")
+library("jpeg")
+f1 = readJPEG("Sampleimages/4143211.jpg")
+print(f1)
+t=calculateOtsu(as.vector(f1))
+print(t)
+
